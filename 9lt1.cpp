4d76e6d95e96b1e57a3ce425101f219fec1d87b3
Võ Thanh Nguyên <nguyenvo9955@gmail.com>
2024-12-22 18:21:01 +0700

    Add files via upload

--- 9lt1.cpp
+++ 9lt1.cpp
@@ -1,43 +1,30 @@
 #include <stdio.h>
-#define MAX 100
 
 int main() {
-    int arr[MAX];
-    int len = 0;
-    int n, val, pos;
+    int arr[100], n, val, pos;
 
-    printf("Nhap so phan tu muon nhap (toi da %d): ", MAX);
+    printf("Nhap so phan tu: ");
     scanf("%d", &n);
-    if (n < 0 || n > MAX) {
-        printf("So phan tu khong hop le!\n");
-        return 1;
-    }
-    printf("Nhap %d phan tu:\n", n);
     for (int i = 0; i < n; i++) {
-        printf("Phan tu thu %d: ", i);
+        printf("Nhap phan tu thu %d: ", i);
         scanf("%d", &arr[i]);
     }
-    len = n;
     printf("Nhap gia tri muon them: ");
     scanf("%d", &val);
-    printf("Nhap vi tri muon them (0 - %d): ", MAX - 1);
+    printf("Nhap vi tri muon them: ");
     scanf("%d", &pos);
-    if (pos < 0 || pos >= MAX) {
-        printf("Vi tri khong hop le!\n");
-        return 1;
-    }
-    if (pos >= len) {
-        arr[pos] = val;
-        len = pos + 1;
-    } else {
-        for (int i = len; i > pos; i--) {
+    if (pos >= 0 && pos <= n) {
+        for (int i = n; i > pos; i--) {
             arr[i] = arr[i - 1];
         }
         arr[pos] = val;
-        len++;
+        n++;
+    } else {
+        printf("Vi tri khong hop le!\n");
+        return 1;
     }
     printf("Mang sau khi them phan tu:\n");
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
